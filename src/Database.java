f9a11542f794a1a09f3c865bdbed06f3fc7a5b5d
EMers <egor.merzlov@bk.ru>
2021-07-09 13:32:55 +0300

    command

---
+++ src/Database.java
@@ -0,0 +1,22 @@
+public class Database {
+    public void inset(){
+
+        System.out.println("Inserting....");
+    }
+
+    public void update(){
+
+        System.out.println("Update....");
+    }
+
+    public void select(){
+
+        System.out.println("Reading....");
+    }
+
+    public void delete(){
+
+        System.out.println("Delete.....");
+    }
+
+}
